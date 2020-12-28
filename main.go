d92176cec1ff6815086d9c80f8bf509c77fdd714
Zsolt Huba <hubidubi@gmail.com>
2020-12-28 10:56:18 +0100

    timeout configuration

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 	"log"
 	"main/app"
 	"net/http"
+	"time"
 )
 
 func main() {
 	port := flag.String("port", "8080", "port to use")
+	timeout := flag.String("timeout", "10", "timeout value")
+	duration, _ := time.ParseDuration(*timeout + "s")
 	dsn := flag.String("dsn", "gocontacts:gocontacts@tcp(localhost:3306)/gocontacts?charset=utf8&parseTime=true", "DSN to use")
 	flag.Parse()
+	log.Printf("Timeout set to %ss", *timeout)
 
 	if DB, err := gorm.Open(mysql.Open(*dsn), &gorm.Config{}); err == nil {
+		log.Print("DB connected: " + *dsn)
 		DB.AutoMigrate(&app.Contact{})
 		app.DB = DB
 	} else {
@@ -22,8 +27,14 @@ func main() {
 		return
 	}
 	log.Print("Go contacts is running on port " + *port)
-	err := http.ListenAndServe(":"+*port, app.GetRouter())
-	if err != nil {
+	srv := &http.Server{
+		Handler:      app.GetRouter(),
+		Addr:         ":" + *port,
+		ReadTimeout:  duration,
+		WriteTimeout: duration,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal("error starting http server :: ", err)
 		return
 	}
